0c19d41eec7da66381a36b24e32dbbfd4f68b457
Yago Azevedo <yago.dev@gmail.com>
2022-05-10 19:52:02 -0300

    ajustes aula 10-03

--- exemplos/17-seletores-e-modificadores-js/index.js
+++ exemplos/17-seletores-e-modificadores-js/index.js
@@ -1,21 +1,44 @@
-function cadastrarProfessor () {
+function cadastrarProfessor (event) {
+    event.preventDefault();
     // Resgatar valores digitados nos inputs
     const nome = document.getElementById("input-nome").value;
     const materia = document.getElementById("input-materia").value;
+
+    if (validarNome(nome)) {
+        // Criando novas colunas
+        let colunaNome = document.createElement("td");
+        let colunaMateria = document.createElement("td");
     
-    // Criando novas colunas
-    let colunaNome = document.createElement("td");
-    let colunaMateria = document.createElement("td");
+        // Adicinando valores digitados nas colunas
+        colunaNome.innerText = nome;
+        colunaMateria.innerText = materia;
+    
+        // Criando linha e adicionando colunas (filhos)
+        const linha = document.createElement("tr");
+        linha.appendChild(colunaNome);
+        linha.appendChild(colunaMateria);
+    
+        // Adicionando linha na tabela
+        document.querySelector("#conteudo-tabela").appendChild(linha);
+    
+        limparCampos();
+        document.getElementById("input-nome").focus();
+    }
+}
 
-    // Adicinando valores digitados nas colunas
-    colunaNome.innerText = nome;
-    colunaMateria.innerText = materia;
+function limparCampos() {
+    document.getElementById("input-nome").value = "";
+    document.getElementById("input-materia").value = "";
+}
 
-    // Criando linha e adicionando colunas (filhos)
-    const linha = document.createElement("tr");
-    linha.appendChild(colunaNome);
-    linha.appendChild(colunaMateria);
-
-    // Adicionando linha na tabela
-    document.querySelector("#conteudo-tabela").appendChild(linha);
+function validarNome(nome) {
+    // validação nome vazio
+    if (!nome) {
+        // colocando o input com borda vermelha
+        document.getElementById("input-nome").setAttribute('class', 'input-com-erro form-control');
+        document.querySelector(".campo-obrigatorio").style.display = "block";
+        return false;
+    } else {
+        return true;
+    }
 }
