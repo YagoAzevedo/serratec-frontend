b565042312c4fc0e9f68f8100141348ce5109f91
Yago Azevedo <yago.dev@gmail.com>
2022-05-10 21:17:21 -0300

    fetch

--- exemplos/17-seletores-e-modificadores-js/index.js
+++ exemplos/17-seletores-e-modificadores-js/index.js
@@ -1,3 +1,27 @@
+window.onload = function() {
+    carregarProfessores();
+}
+
+function carregarProfessores() {
+    fetch("https://randomuser.me/api/?results=5").then(resultado => {
+        return resultado.json();
+    }).then(response => {
+        response.results.forEach(professor => {
+            let colunaNome = document.createElement("td");
+        
+            // Adicinando valores digitados nas colunas
+            colunaNome.innerText = professor.name.first;
+        
+            // Criando linha e adicionando colunas (filhos)
+            const linha = document.createElement("tr");
+            linha.appendChild(colunaNome);
+        
+            // Adicionando linha na tabela
+            document.querySelector("#conteudo-tabela").appendChild(linha);
+        });
+    })
+}
+
 function cadastrarProfessor (event) {
     event.preventDefault();
     // Resgatar valores digitados nos inputs
